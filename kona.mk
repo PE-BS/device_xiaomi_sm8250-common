fb0904e8d33b7987abc0637c1d09b507f365bbb3
maytinhdibo <iammaytinhdibo@gmail.com>
2021-07-24 12:20:09 +0200

    sm8250-common: parts: Ignore check while wakeup by call
    Signed-off-by: maytinhdibo <iammaytinhdibo@gmail.com>

--- kona.mk
+++ kona.mk
@@ -119,6 +119,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     XiaomiParts
 
+PRODUCT_COPY_FILES += \
+     $(LOCAL_PATH)/parts/privapp-permissions-parts.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-parts.xml
+
 #Display
 $(call inherit-product, $(LOCAL_PATH)/kona/display/display.mk)
     
