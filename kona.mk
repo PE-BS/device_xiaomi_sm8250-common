027f8ee60cb76440a5ca4ce8d51452d77e489a31
blankaf <realblankaf@gmail.com>
2021-08-07 14:46:02 +0200

    sm8250-common: Update blobs from nio (LA.UM.9.12.r1-10800-SMxx50.0)

--- kona.mk
+++ kona.mk
@@ -85,6 +85,7 @@ $(call inherit-product, $(LOCAL_PATH)/kona/audio/audio.mk)
 #Bluetooth
 PRODUCT_PACKAGES += \
     vendor.qti.hardware.bluetooth_audio@2.0.vendor \
+    vendor.qti.hardware.bluetooth_audio@2.1.vendor \
     vendor.qti.hardware.btconfigstore@1.0.vendor \
     vendor.qti.hardware.btconfigstore@2.0.vendor
      
