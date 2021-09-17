d922ee93ce97630a65d70d2da345b6fc804b9c5b
blankaf <realblankaf@gmail.com>
2021-09-17 16:39:28 +0200

    sm8250-common: Build livedisplay again
    This reverts commit 9157045d1bfd1166a81885b9a1ae59d0bc6f9202.

--- kona.mk
+++ kona.mk
@@ -181,6 +181,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     lights.kona \
     android.hardware.lights-service.qti
+    
+# LiveDisplay
+PRODUCT_PACKAGES += \
+    vendor.lineage.livedisplay@2.0-service-sdm
 
 # Low power Whitelist
 PRODUCT_COPY_FILES += \
